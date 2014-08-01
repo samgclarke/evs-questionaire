d0c046a47eabae3ea078a312dfbfda12750b1ae6
Sam Clarke <sam@rivalschools.com>
2014-08-01 11:55:33 -0700

    hook up firebase

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['firebase']);
 
-myApp.controller('formCtrl', function ($scope) {
-    $scope.foo = 'bar';
+myApp.controller('formCtrl', function ($scope, $firebase) {
+    // firebase
+    var ref = new Firebase("https://evs.firebaseio.com/");
+    var sync = $firebase(ref);
+    var formsArray = sync.$asArray();
+
+    $scope.forms = formsArray;
+
+    console.log('forms', formsArray);
+
+    // array of questions
+    $scope.questions = [
+        "",
+        "What is a fish?",
+    ];
+
+    $scope.submitForm = function (form) {
+        console.log('You sumbitted: ', form);
+        formsarray.$add(form).then(function (newChildRef) {
+            console.log("added record with id " + newChildRef.name());
+        });
+        console.log('forms', forms);
+        $scope.form = {};      
+    };
 });
